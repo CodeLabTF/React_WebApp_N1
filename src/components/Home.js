9a178da4f7b14a2f4cf4ae546cb83891cffa2102
CodeLabTF <ft_rg_zen@proton.me>
2024-01-04 18:58:14 +0200

    No22

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,18 @@ import { useState} from 'react';
 import "./Home.css"
 const Home = () => {
 	const [blogs, setBlogs] = useState([
-		{ title: '1. My new website', body: 'Rand txt', author: 'mario', id: 1},
-		{ title: '2. Welcome Party', body: 'Rand txt', author: 'yoshi', id: 2},
-		{ title: '3. Top tips', body: 'Rand txt', author: 'josie', id: 3},
+		{ title: '1. My new website', body: 'Rand txt', author: 'dave', id: 1},
+		{ title: '2. Welcome Party', body: 'Rand txt', author: '', id: 2},
+		{ title: '3. Top tips', body: 'Rand txt', author: 'marika', id: 3},
 		
 	]);
 
 //  Data is stored in useState!
 
+// In the map function "blog" is a random arguemnt is used to asign array values!
 	return (
 		<div className="home">
 
-		//In the map function blog is a randomly arguemnt that we can choose and
-		//it is used to asign array values!
 		{blogs.map((blog) => (
 			<div className="blog-preview" key={blog.id}>
 				<h2> {blog.title} </h2>
