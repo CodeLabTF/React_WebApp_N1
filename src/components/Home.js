aefab3475627b8f895cceb533224e862b5aeff3e
CodeLabTF <ft_rg_zen@proton.me>
2024-01-30 15:34:25 +0200

    No323

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,47 @@
 
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import "./Home.css"
+import BlogList from './BlogList';
+
 const Home = () => {
 	const [blogs, setBlogs] = useState([
 		{ title: '1. My new website', body: 'Rand txt', author: 'dave', id: 1},
-		{ title: '2. Welcome Party', body: 'Rand txt', author: '', id: 2},
+		{ title: '2. Welcome Party', body: 'Rand txt', author: 'kalev', id: 2},
 		{ title: '3. Top tips', body: 'Rand txt', author: 'marika', id: 3},
-		
-	]);
+
+]);
+
+
+const [name, setName] = useState('dave');
+
+	useEffect( () => {
+	console.log('useEffect ran')
+
+	console.log(name)
+	}, 
+	[name ]);
+
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter(blog => blog.id !== id);
+		setBlogs(newBlogs);
+	}
+	
 
 //  Data is stored in useState!
 
 // In the map function "blog" is a random arguemnt is used to asign array values!
+
+// blogs is a prop!
 	return (
 		<div className="home">
 
-		{blogs.map((blog) => (
-			<div className="blog-preview" key={blog.id}>
-				<h2> {blog.title} </h2>
-				<p> Written by {blog.author} </p>
-			 </div>	
-		)) }
+		<BlogList blogs= {blogs} title="A Blog" handleDelete={handleDelete}/>		
+		<button onClick={() => setName('karen') } > change name </button>
+		<p> {name} </p>
 
-		</div> 
-	)
+	
+		</div>   
+			)
 
      
 }
